d7b212e886071e6580f285fca239b2c8c4705ec9
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-10-25 14:56:52 +0900

    I have done up to page 394.

---
+++ MyCamera/EffectView.swift
@@ -0,0 +1,18 @@
+//
+//  EffectView.swift
+//  MyCamera
+//
+//  Created by apple on 2024/10/04.
+//
+
+import SwiftUI
+
+struct EffectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EffectView()
+}
